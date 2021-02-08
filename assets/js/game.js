a122aa1873886e78bd42d9a8fe6a982f8add00cf
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-07 16:47:45 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,11 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -22,7 +22,7 @@ var fight = function() {
         enemyHealth = enemyHealth - playerAttack;
 
         // Log a resulting message to the console so we know that it worked.
-        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining");
+        console.log(playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has " + enemyHealth + " health remaining");
 
         // check enemy's health
         if (enemyHealth <= 0) {
@@ -36,7 +36,7 @@ var fight = function() {
         playerHealth = playerHealth - enemyAttack;
 
         // Log a resulting message to the console so we know that it worked.
-        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining");
+        console.log(enemyNames[i] + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining");
 
         // check player's health
         if (playerHealth <= 0) {
@@ -65,4 +65,8 @@ var fight = function() {
     }
 };
 
-fight();
+for(var i=0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+//fight();
