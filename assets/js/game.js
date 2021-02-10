6239847210ff1a83152ff1372f6c197b72dfe0f9
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-10 09:27:02 -0800

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,14 +23,17 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack -3, playerAttack);
+    
+    enemyHealth = Math.max(0, enemyHealth - damage);
+    
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -48,8 +51,11 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     };
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    // generate random damage value based on enemy's attack power
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
+
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -74,7 +80,7 @@ var startGame = function() {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i +1) );
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
 
       // if we're not at the last enemy in the array AND player not dead
@@ -159,6 +165,12 @@ var shop = function() {
       shop();
       break;
   }
+};
+
+var randomNumber = funtion(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+  return value;
 };
 
 startGame();
