58ff965d1afc8ad23b960aa24a2124259d018919
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-10 11:57:12 -0800

    update shop() function prompt for less typing

--- assets/js/game.js
+++ assets/js/game.js
@@ -129,20 +129,18 @@ var endGame = function () {
 var shop = function() {
   //ask player what they'd like to do?
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', OR 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
 
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
-    case "refill":
-    case "REFILL":
+    case 1:
       playerInfo.refillHealth();
       break;
-    case "upgrade":
-    case "UPGRADE":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case "leave":
-    case "LEAVE":
+    case 3:
       window.alert("Leaving the store.")
       break;
     default:
